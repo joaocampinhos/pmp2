c4b24ab6c9df57c9ef0b63389758573b4d95bb84
pefido <pedrofilipeduraes@hotmail.com>
2014-11-10 16:43:00 +0000

    starting

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -1,1 +1,15 @@
-print(true);
+object t2 {
+  
+  def probA(input: String, output: String): Unit = {
+    
+  }
+  
+  
+  
+    
+    def main(args: Array[String]) {
+      
+      println(args(0) + " ; " + args(1));
+      probA(args(0), args(1));
+  }
+}
