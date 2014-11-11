dcb4e26aeaa2a8ea2e5d1f960f54f72d06b8ee76
pefido <pedrofilipeduraes@hotmail.com>
2014-11-11 15:55:57 +0000

    funca?

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -2,6 +2,8 @@ object t2 {
   
   def probA(input: String, output: String): Unit = {
     
+    val x = scala.xml.XML.loadFile(input);
+    println(x.text);
   }
   
   
@@ -9,7 +11,9 @@ object t2 {
     
     def main(args: Array[String]) {
       
-      println(args(0) + " ; " + args(1));
+      //println(args(0) + " ; " + args(1));
       probA(args(0), args(1));
+      //val foo = <foo><bar type="greet">hi</bar><bar type="count">1</bar><bar type="color">yellow</bar></foo>
+      //println(foo.text);
   }
 }
