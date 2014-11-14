ce3e1806f3f88fcd52bce286eae0e04e3d1525ba
joaocampinhos <joao.campinhos@gmail.com>
2014-11-14 20:16:20 +0000

    jantar

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -1,102 +1,58 @@
 object Proj02 {
-  
-  def probA(input: String, output: String): Unit = {
-    
-    var x = scala.xml.XML.loadFile(input);
-
-    var files = 0;
-    var interfaces = 0;
-    var classes = 0;
-
-
-    class File(name: String){
-    	var nome = name;
-    	var classList = List[String]();
-    	var interfaceList = List[String]();
-
-    	def addClass(name: String): Unit = {
-    		classList = name :: classList;
-    	}
-
-    	def addInterface(name: String): Unit = {
-    		interfaceList = name :: interfaceList;
-    	}
-
-    	def getName(): String = {
-    		return nome;
-    	}
-
-    	def getClasses(): List[String] = {
-    		return classList;
-    	}
-
-    	def getInterfaces(): List[String] = {
-    		return interfaceList;
-    	}
-    }
-
-    var fileList = List[File]();
-
-
-    //isto conta o numero de ficheiros, classes e interfasses e guarda na lista
-    var program = x \\ "java-source-program";
-    for(file <- program \ "java-class-file"){
-    	files = files + 1;
-    	fileList = (new File((file \ "@name").text)) :: fileList;
-    	for(interface <- file \ "interface"){
-    		interfaces = interfaces + 1;
-    		fileList(files-1).addInterface((interface \ "@name").text);
-    	}
-    	for(clace <- file \ "class"){
-    		classes = classes + 1;
-    		fileList(files-1).addClass((clace \ "@name").text);
-    	}
-    }
-
-
-    //testar se ta a guardar bem
-    /*for(file <- fileList){
-    	println(file.getName() + "::");
-    	for(interface <- file.getInterfaces()){
-    		println(interface);
-    	}
-    	for(clace <- file.getClasses()){
-    		println(clace);
-    	}
-    }*/
-
-	var htmlFiles = new xml.NodeBuffer;
-	for(file <- fileList){
-    	htmlFiles &+ <h3>ficheiro {file.getName()}</h3>;
-    	for(interface <- file.getInterfaces()){
-    		htmlFiles &+ <h3>interface {interface}</h3>;
-    	}
-    	for(clace <- file.getClasses()){
-    		htmlFiles &+ <h3>class {clace}</h3>;
-    	}
-    }
-
-
-    var html = <html>
-    		<h3>{files} ficheiro(s)</h3>
-    		<h3>{interfaces} interface(s)</h3>
-    		<h3>{classes} classe(s)</h3>
-    		{htmlFiles}
-    	</html>;
-
-
-    scala.xml.XML.save(output, html);
-  }
 
   def probB(input: String, output: String): Unit = {
 
+    var cName = ""; //Esta solução é um bocado feia não?
+
+    def profile( node: xml.Node) : xml.Node = {
+
+      def sub( seq : Seq[xml.Node]) : Seq[xml.Node] =
+        for( subNode <- seq ) yield profile( subNode )
+
+      node match {
+        case <java-source-program>{ el @ _* }</java-source-program> =>
+          <java-source-program>{ sub(el) }</java-source-program>
+        case a @ <java-class-file>{ el @ _* }</java-class-file> =>
+          println(a \ "_")
+          <java-class-file>{ sub(el) }</java-class-file> //Continuar com o nome do ficheiro. ficou perdido pelo caminho
+        case a @ <class>{ el @ _* }</class> =>
+          cName = (a \ "@name").text
+          <class>{ sub(el) }</class> //Tamos a perder cenas importantes!
+        case a @ <method>{ el @ _* }</method> if (a \ "@name").text == "main" =>
+          addList(a);
+        case a @ <method>{ el @ _* }</method> =>
+          var fullname = cName+"."+(a \ "@name").text
+          println(fullname)
+          addCount(a, fullname);
+        case other @ _ => other
+      }
+    }
+
+    def addCount( seq: Seq[xml.Node], name: String) : xml.Node = {
+      <send message="count">
+        <target>
+          CentralCounting
+        </target>
+        <arguments>
+          String -> {name}
+        </arguments>
+      </send>
+    }
+
+    def addList( seq: Seq[xml.Node]) : xml.Node = {
+      <send message="list">
+        <target>
+          CentralCounting
+        </target>
+        <arguments/>
+      </send>
+    }
+
+    var x = scala.xml.XML.loadFile(input);
+    scala.xml.XML.save(output, profile(x));
   }
-  
-  
-  
-    
-    def main(args: Array[String]) {
-      
-      probA(args(0), args(0).dropRight(9) + ".stats.html");
+
+  def main(args: Array[String]) {
+    probB(args(0), args(0).dropRight(9) + ".prof.xml");
   }
 }
