11218af3b68d0674c67110b48d8aa06365354cdc
joaocampinhos <joao.campinhos@gmail.com>
2014-11-16 12:16:30 +0000

    xml acertado. falta resolver o problema dos nomes a desaparecer

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -2,8 +2,8 @@ object Proj02 {
 
   def probB(input: String, output: String): Unit = {
 
-    var cName = ""; //Esta solução é um bocado feia não?
-    var mName = "";
+    var fullName = ""; //Uma solução um bocado feia
+    var main = false;
 
     def profile( node: xml.Node) : xml.Node = {
 
@@ -14,15 +14,15 @@ object Proj02 {
         case <java-source-program>{ el @ _* }</java-source-program> =>
           <java-source-program>{ sub(el) }</java-source-program>
         case a @ <java-class-file>{ el @ _* }</java-class-file> =>
-          println(a \ "_")
           <java-class-file>{ sub(el) }</java-class-file> //Continuar com o nome do ficheiro. ficou perdido pelo caminho
         case a @ <class>{ el @ _* }</class> =>
-          cName = (a \ "@name").text
+          fullName = (a \ "@name").text
           <class>{ sub(el) }</class> //Tamos a perder cenas importantes!
         case a @ <method>{ el @ _* }</method> if (a \ "@name").text == "main" =>
-          <method>{ sub(el) }</method>
+          main = true
+          <method>{ sub(el) }</method> //Mais cenas perdidas :(
         case a @ <method>{ el @ _* }</method> =>
-          mName = (a \ "@name").text
+          fullName += "." + (a \ "@name").text
           <method>{ sub(el) }</method>
         case <block>{ el @ _* }</block> =>
           <block>{ addProf()}{ sub(el) }</block>
@@ -31,20 +31,20 @@ object Proj02 {
     }
 
     def addProf() : xml.Node = {
-      if (mName == "")
+      if (main)
         <send message="list">
           <target>
-            CentralCounting
+            <var-ref name="CentralCounting"/>
           </target>
           <arguments/>
         </send>
       else
         <send message="count">
           <target>
-            CentralCounting
+            <var-ref name="CentralCounting"/>
           </target>
           <arguments>
-            String -> {cName+"."+mName}
+            <literal-string value="{fullName}"/>
           </arguments>
         </send>
     }
